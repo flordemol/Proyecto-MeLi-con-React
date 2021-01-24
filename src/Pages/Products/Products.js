33254b2dc94313e9ffaec00399bba9a893887e4d
Maria Florencia De Mollein <florenciademollein@gmail.com>
2021-01-24 01:23:53 -0300

    Peticion HTTP

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,6 +1,28 @@
+import { useEffect, useReducer } from 'react';
+import { initialState, productsReducer } from './../../reducers/products';
+import { FETCHING, FETCH_SUCCESS } from './../../reducers/actions/products';
+import {BASE_URL} from './../../constants';
+import axios from 'axios';
 import PropTypes from "prop-types";
 
 const Products = ({search}) => {
+    
+    const [state, dispatch] = useReducer(productsReducer, initialState);
+
+    const getProducts = async () => {
+        try {
+            const {data : info} = await axios.get(`${BASE_URL}/sites/MLA/search?q=${search}&limit=8`);
+            dispatch({type: FETCH_SUCCESS, payload: {data : info.results}});
+            console.log(info);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    useEffect(() => {
+        // Petición HTTP
+        getProducts();
+    }, [search]);
     
     return ( 
         <>
